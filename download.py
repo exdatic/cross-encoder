548e9611bc824263b028d5634263f45557f5fef5
Mark Reinke <54356376+loopdeloop76@users.noreply.github.com>
2023-01-27 16:02:44 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,13 +1,12 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
-
-# In this example: A Huggingface BERT model
-
-from transformers import pipeline
+from sentence_transformers import CrossEncoder
+import torch
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    device = 0 if torch.cuda.is_available() else -1
+    model = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2', device=device)
 
 if __name__ == "__main__":
     download_model()
